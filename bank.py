a968c431dea1b2a651bc68a490b4a3512f367d76
danielkim321 <snyderl27@chcstudent.com>
2025-10-01 11:31:18 -0400

    Update bank.py

---
+++ bank.py
@@ -0,0 +1,6 @@
+print("Welcome to the bank")
+
+students = ["john","zach","Paul","mary","susan"]
+balances = [100.50,200.75,50.25,300.00,150.00]
+for i in range(len(students)):
+    print(f"Student: {students[i]} Balance: {balances[i]}")
