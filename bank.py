d7bc8212a5cfebf45a30486accd6add434577fdf
danielkim321 <snyderl27@chcstudent.com>
2025-10-01 11:32:17 -0400

    Update bank.py

--- bank.py
+++ bank.py
@@ -1,5 +1,7 @@
 print("Welcome to the bank")
-
+print("1. View Balance")
+print("2. Deposit")
+print("3. Withdraw")
 students = ["john","zach","Paul","mary","susan"]
 balances = [100.50,200.75,50.25,300.00,150.00]
 for i in range(len(students)):
